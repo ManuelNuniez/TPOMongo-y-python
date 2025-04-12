12bccb52b052c7d7d44a123695cc5b382e1a39f0
Santiago Ribecca <ribeccasantiago@gmail.com>
2025-04-12 16:57:25 -0300

    wip: parte 2

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def parte_uno():
     print("\nListando todas las Colecciones")
     print(db.list_collection_names())
 
+    # Eliminar base de datos de pruebas
+    client.drop_database('baseDeDatos')
+    
     # Creamos Nueva BD de prueba:
 
     print("\nCreando base de datos 'baseDeDatos'")
@@ -15,8 +18,6 @@ def parte_uno():
     print("\nCreando coleccion 'coleccion'")
     test_db.create_collection('coleccion')
 
-    # Eliminar base de datos de pruebas
-    client.drop_database('baseDeDatos')
 
 # MostrarDocumentos
 def MostrarTodosLosDocumentos():
@@ -28,5 +29,38 @@ def MostrarTodosLosDocumentos():
 #     #parte_uno()
 #     parte_dos()
 
+def filters():
+    print("\n1.Recetas que usen 'leche'")
+    response = list(db.recetas.find({"ingredientes.nombre": "Leche"}))
+    for recipe in response:
+        print(recipe["nombre"])
+    
+    print("\n2.Recetas con un tiempo de coccion menor a 30 minutos")
+    response = list(db.recetas.find({"tiempo_coccion": {"$lt": 30}}))
+    for recipe in response:
+        print(recipe["nombre"], "-->" ,recipe["tiempo_coccion"], "minuto/s")
+    
+    print("\n3.Recetas que sean de la categoria 'Sopas'")
+    response = list(db.recetas.find({"categoria": "Sopas"}))
+    for recipe in response:
+        print(recipe["nombre"])
+    
+    print("\n4.Recetas para mas de 5 personas")
+    response = list(db.recetas.find({"rendimiento.cantidad": {"$gt": 5}}))
+    for recipe in response:
+        print(recipe["nombre"])
+
+    print("\n5.Recetas que sean de dificultad 'Media'")
+    response = list(db.recetas.find({"dificultad": "Medio"}))
+    for recipe in response:
+        print(recipe["nombre"])
+
+    # print("\n6.Recetas que no usen 'cebolla'")
+    # response = list(db.recetas.find({"ingredientes.nombre": {"$ne": "Cebolla"}}))
+    # for recipe in response:
+    #     print(recipe["nombre"])
+    
+
 #parte_uno()
-MostrarTodosLosDocumentos()
+#MostrarTodosLosDocumentos()
+filters()
